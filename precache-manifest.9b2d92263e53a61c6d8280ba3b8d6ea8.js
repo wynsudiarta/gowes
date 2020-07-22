114c966df761a05ce7d82e3cf65c3ee69c64be45
Wayan Sudiarta <wayan@tix.com.au>
2020-07-22 14:13:16 +0800

    Updates

---
+++ precache-manifest.9b2d92263e53a61c6d8280ba3b8d6ea8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c603d33a2c7be36130b2",
+    "url": "/gowes/static/css/main.6630e204.chunk.css"
+  },
+  {
+    "revision": "c603d33a2c7be36130b2",
+    "url": "/gowes/static/js/main.c603d33a.chunk.js"
+  },
+  {
+    "revision": "3581ebf7e463fce27c8c",
+    "url": "/gowes/static/js/runtime~main.3581ebf7.js"
+  },
+  {
+    "revision": "0ad867cae66ddd60d0c7",
+    "url": "/gowes/static/js/2.0ad867ca.chunk.js"
+  },
+  {
+    "revision": "bee2192111c2607a0301de6bb3a4b6f2",
+    "url": "/gowes/static/media/pic.bee21921.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gowes/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1783cab2509209a4700e9b7eec07f03f",
+    "url": "/gowes/index.html"
+  }
+];
